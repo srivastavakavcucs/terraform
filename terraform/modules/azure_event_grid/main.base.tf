b3380d935fe7ba001906db2894a93edd37ac9779
srivastavakavcucs <srivastavak@vystarcu.org>
2026-05-21 03:38:47 -0400

    feat: add main.base.tf for azure_event_grid module

---
+++ terraform/modules/azure_event_grid/main.base.tf
@@ -0,0 +1,26 @@
+#------------------------------------------------------------------------
+# Use the IaC Common Variables Module for validations and outputs.
+#------------------------------------------------------------------------
+
+module "base" {
+  source = "../iac_base"
+
+  # Required Variables
+  app_name                  = var.app_name
+  region                    = var.region
+  component_name            = "evtgrd"
+  environment               = var.environment
+  environment_number_suffix = var.environment_number_suffix
+  common_tags               = var.common_tags
+
+  # Optional Variables
+  enable_telemetry           = var.enable_telemetry
+  lock                       = var.lock
+  resource_tags              = var.resource_tags
+  custom_resource_group_name = var.custom_resource_group_name
+
+  # Providers
+  providers = {
+    azurerm = azurerm
+  }
+}
